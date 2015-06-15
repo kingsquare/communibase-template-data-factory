c2cd160b85e2627a697b36e44c8046a31897a6b9
DavidRip <david@kingsquare.nl>
2015-06-15 16:32:32 +0200

    bugfix

--- entityType/Event.js
+++ entityType/Event.js
@@ -1,3 +1,15 @@
+'use strict';
+
+var BaseSerializer = require('./Base.js');
+var moment = require('moment');
+
 module.exports = {
-	titleFields: ['title','startDate']
+	titleFields: ['title','startDate'],
+	composeTitle: function (chunks, entityTitle, event) {
+		if (chunks && chunks[1]) {
+			chunks[1] = '(' + moment(chunks[1]).format('D-M-YYYY') + ')';
+		}
+
+		return BaseSerializer.composeTitle.apply(this, arguments);
+	}
 };
--- entityType/PeriodicTariffList.js
+++ entityType/PeriodicTariffList.js
@@ -5,7 +5,7 @@ var moment = require('moment');
 module.exports = {
 	_getTitlePromise: function (titleFields, entityTypeTitle, periodTariffList) {
 		var result = periodTariffList.endOfTariff ?
-			'Tarievenlijst t/m ' + moment(periodTariffList.endOfTariff).format('DD-MM-YYYY') :
+			'Tarievenlijst t/m ' + moment(periodTariffList.endOfTariff).format('D-M-YYYY') :
 			'Tarievenlijst zonder einddatum';
 		return Promise.resolve([result]);
 	}
