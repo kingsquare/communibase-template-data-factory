c84bbb9ba408bd6e0f5545e5a6e35a88645013e2
DavidRip <david@kingsquare.nl>
2015-06-15 16:33:36 +0200

    added commentary

--- entityType/Event.js
+++ entityType/Event.js
@@ -5,6 +5,13 @@ var moment = require('moment');
 
 module.exports = {
 	titleFields: ['title','startDate'],
+
+	/**
+	 * https://trello.com/c/aSUzT0Ai/1330-verbetering-gebruikservaring-naar-evenement-slepen-naast-evenement-titel-ook-
+	 *
+	 * @param {Array} chunks
+	 * @return {string}
+	 */
 	composeTitle: function (chunks, entityTitle, event) {
 		if (chunks && chunks[1]) {
 			chunks[1] = '(' + moment(chunks[1]).format('D-M-YYYY') + ')';
