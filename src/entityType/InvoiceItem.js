4fee461fee13f15d09ad0e2de3e2d49c389baa13
Tim de Koning <git@king-management.nl>
2018-03-14 12:02:34 +0100

    Expose tax for invoiceItem

--- src/entityType/InvoiceItem.js
+++ src/entityType/InvoiceItem.js
@@ -8,16 +8,20 @@ module.exports = {
     const allVariablesAreRequested = (requestedPaths.length === 1 && requestedPaths[0].substring(0, 1) === '#');
 
     return BaseSerializer.getPromiseByPaths.apply(this, arguments).then((templateData) => {
-      const requestedTotalsVariables = helpers.getRequestedSubVariables(requestedPaths, 'totals');
+      const requestedTotalsVariables = helpers.getRequestedSubVariables(requestedPaths, 'totals').map(
+        requestedPath => requestedPath.split('.')[0]
+      );
       const incltaxMultiplier = (document.taxPercentage ? ((100 + document.taxPercentage) / 100) : 1);
       const totals = {
         ex: document.quantity * document.pricePerUnit,
+        tax: document.quantity * document.pricePerUnit * (incltaxMultiplier - 1),
         in: document.quantity * document.pricePerUnit * incltaxMultiplier,
         perUnitEx: document.pricePerUnit,
         perUnitIn: document.pricePerUnit * incltaxMultiplier
       };
 
       totals.exRounded = helpers.round(totals.ex, 2);
+      totals.taxRounded = helpers.round(totals.tax, 2);
       totals.inRounded = helpers.round(totals.in, 2);
       totals.perUnitExRounded = helpers.round(totals.perUnitEx, 2);
       totals.perUnitInRounded = helpers.round(totals.perUnitIn, 2);
